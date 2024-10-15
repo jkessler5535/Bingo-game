fdfc5db3eb5965cf7a62e9cf626f681b34a74215
jacktrusler <jtrus93@gmail.com>
2024-10-15 10:50:03 -0400

    styling login screen, adding errors

--- src/components/login.js
+++ src/components/login.js
@@ -1,76 +1,87 @@
-import React, {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import HeaderImg from '../assets/images/header-img';
 import BingoIcon from '../assets/images/bingo-logo';
 import Sponsors from '../assets/images/sponsors';
 
-
-
 const LoginPage = () => {
   const [twitterHandle, setTwitterHandle] = useState('');
+  const [telegram, setTelegram] = useState('');
+  const [error, setError] = useState('');
   const navigate = useNavigate();
 
-  const handleInputChange = (e) => {
-    setTwitterHandle(e.target.value);
-  };
+  useEffect(() => {
+    const twitter = localStorage.getItem("twitterHandle")
+    const tg = localStorage.getItem("telegram")
+
+    if (twitter && tg) {
+      navigate('/bingo')
+    }
+  }, [navigate])
+
 
   const handleLogin = (e) => {
     e.preventDefault();
 
-    if (twitterHandle) {
+    if (twitterHandle && telegram) {
       localStorage.setItem('twitterHandle', twitterHandle);
-    
+      localStorage.setItem('telegram', telegram);
+      setError("");
+      navigate('/game-directions');
+    } else {
+      setError("Both inputs must be filled");
+    }
+  };
 
-    navigate('./game-directions');
-  }else{
-    alert('Please enter your Twitter handle');
-  }
-};
-
-  return(
+  return (
     <div className="login-page container">
-      <HeaderImg/>
+      <HeaderImg />
       <div className="line top-line"></div>
-      <BingoIcon/>
+      <BingoIcon />
       <h1 className="welcome">Welcome To Blocktail's Bingo Game</h1>
-      
+
 
       <form onSubmit={handleLogin} className="login-form-container">
-          <h2>Sign in to play</h2>
-        <label htmlFor="wallet-address" className="twitter-handle-label">
-          Wallet Address
+        <h2>Sign in to play</h2>
+        <label htmlFor="telegram" className="login-label">
+          Telegram
         </label>
         <input
           type="text"
-          id="wallet-address"
-          name="wallet-address"
-          placeholder="0x329sdfadsghbngvdgb"
-          className="twitter-handle-input"
-          />
+          name="telegram"
+          placeholder="telegram..."
+          className="login-input"
+          onChange={(e) => setTelegram(e.target.value)}
+        />
 
-        <label htmlFor="twitter-handle" className="twitter-handle-label">
+        <label htmlFor="twitter-handle" className="login-label">
           Twitter Handle
         </label>
         <input
           type="text"
-          id="twitter-handle2"
           name="twitterHandle"
-          placeholder="@twitter_handle"
-          className="twitter-handle-input"
+          placeholder="@twitter_handle..."
+          className="login-input"
           value={twitterHandle}
-          onChange={handleInputChange}
-          />
-          
+          onChange={(e) => setTwitterHandle(e.target.value)}
+        />
+
         <p>All of your information is kept private and we will not share it with anyone.</p>
         <button type="submit" className="login-button">
           Continue
         </button>
-      </form> 
+        {error && (
+          <div className='error-msg'>
+            {error}
+          </div>
+        )}
+
+      </form>
       <div className="line bottom-line">
+      </div>
+      <Sponsors />
     </div>
-    <Sponsors/>
-  </div>
   );
 };
 
